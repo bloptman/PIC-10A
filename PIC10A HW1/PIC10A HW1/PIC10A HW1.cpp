7363049c0cf035073317933ca283e9a313017a80
Brandon Loptman <brandonaloptman@gmail.com>
2020-10-06 23:09:21 -0700

    HW1 files

---
+++ PIC10A HW1/PIC10A HW1/PIC10A HW1.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+
+{
+
+	// This program will display a quote from one of Bikini Bottom's greatest philosophical minds.
+
+	cout << "Hello, world! My name is Brandon.\n"
+
+		//This command makes a space between the lines of text.
+
+		" \n"
+
+		"One of my favorite quotes is:\n"
+
+		" \n"
+
+		"\"The inner machinations of my mind are an enigma.\" ~Patrick Star\n"
+
+		"\n"
+
+		"I like this quote because while on the surface it seems deep and meaningful it is a quote from a children's television show and in actuality means nothing. Honestly, I just wanted to put a SpongeBob quote in an assignment because I thought it would be funny.";
+
+	// This command will make the console stay on the screen indefinitely.
+
+	cin.get();
+
+
+	return 0;
+}
